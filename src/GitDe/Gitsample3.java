47d1ba7c6c53278d97f971480d56539fdd6180c6
amrutatg <105285834+amrutatg@users.noreply.github.com>
2022-05-12 20:42:49 +0530

    Update Gitsample3.java

--- src/GitDe/Gitsample3.java
+++ src/GitDe/Gitsample3.java
@@ -5,8 +5,9 @@ public class Gitsample3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		System.out.println("This is my master branch code");
 		System.out.println("This is my feature branch code");
+		
 	}
 
 }
