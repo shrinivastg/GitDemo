54d8dcac56c4666bc67095cdf3ebcb4f3dc6b821
shrin <shrinivas.tg@gmail.com>
2022-05-12 20:56:23 +0530

    added line from master

--- src/GitDe/Gitsample3.java
+++ src/GitDe/Gitsample3.java
@@ -5,7 +5,7 @@ public class Gitsample3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		System.out.println("This is my master branch code");
 		System.out.println("This is my feature branch code");
 		System.out.println("feature branch code added");
 	}
